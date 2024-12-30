afd6bddbff6d2ec6d1e37b5f55851c0273458347
muralimohanj007 <muralimohanj007@gmail.com>
2024-12-30 12:22:17 +0530

    html

--- app.py
+++ app.py
@@ -33,8 +33,6 @@ def predict_datapoint():
         final_new_data=data.get_data_as_dataframe()
         predict_pipeline=PredictPipeline()
         pred=predict_pipeline.predict(final_new_data)
-        
-        print(pred)
         results=round(pred[0],2)
 
         return render_template('results.html',final_result=results)
